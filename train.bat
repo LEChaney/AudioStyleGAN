32dac3fce6cb103bfb46ec672dc900cd539469b7
FurryFur <lance.e.chaney@gmail.com>
2018-09-20 21:33:31 +1200

    Changed how text embeddings are integrated in the discriminator to be similar to StackGAN.

--- train.bat
+++ train.bat
@@ -2,7 +2,5 @@ CALL activate tensorflow
 python train_wavegan.py train .\train ^
 --data_dir .\data\Final_Datasets\All ^
 --data_first_window ^
---train_batch_size 128 ^
---wavegan_disc_phaseshuffle 0 ^
---wavegan_genr_upsample nn ^
+--wavegan_genr_pp ^
 --train_summary_secs 60
