88c5c7473c14785449446f7b78d258fef70cc6e1
FurryFur <lance.e.chaney@gmail.com>
2018-10-08 00:16:27 +1300

    Fixed supplying incorrect conditional text embeddings to fake discriminator when using historical generated audio clips.

--- ml/wavegan/train_wavegan.py
+++ ml/wavegan/train_wavegan.py
@@ -43,22 +43,26 @@ def train(fps, args):
   args.wavegan_g_kwargs['context_embedding'] = cond_text_embed
   args.wavegan_d_kwargs['context_embedding'] = cond_text_embed
 
-  # Make generator
   with tf.variable_scope('G'):
+    # Make generator
     G_z, c_kl_loss = WaveGANGenerator(z, train=True, **args.wavegan_g_kwargs)
     if args.wavegan_genr_pp:
       with tf.variable_scope('pp_filt'):
         G_z = tf.layers.conv1d(G_z, 1, args.wavegan_genr_pp_len, use_bias=False, padding='same')
-  G_vars = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, scope='G')
 
-  # Make history buffer
-  history_buffer = HistoryBuffer(_WINDOW_LEN, args.train_batch_size * 100, args.train_batch_size)
-  half_batch_from_history = history_buffer.get_from_history_buffer()
-  new_fake_batch = tf.concat([half_batch_from_history, G_z[tf.shape(half_batch_from_history)[0]:]], 0)
-  with tf.control_dependencies([new_fake_batch]):
-    with tf.control_dependencies([history_buffer.add_to_history_buffer(G_z)]):
-      G_z = new_fake_batch
-  G_z.set_shape([args.train_batch_size, _WINDOW_LEN, 1])
+    # Make history buffer
+    history_buffer = HistoryBuffer(_WINDOW_LEN, args.train_batch_size * 100, args.train_batch_size)
+
+    # Select half of batch from history buffer
+    clips_from_history, embeds_from_history = history_buffer.get_from_history_buffer()
+    new_fake_batch = tf.concat([G_z[:tf.shape(G_z)[0] - tf.shape(clips_from_history)[0]], clips_from_history], 0) # Use tf.shape to handle case when clips_from_history is empty
+    new_fake_embeds = tf.concat([cond_text_embed[:tf.shape(cond_text_embed)[0] - tf.shape(embeds_from_history)[0]], embeds_from_history], 0)
+    with tf.control_dependencies([new_fake_batch, new_fake_embeds]):
+      with tf.control_dependencies([history_buffer.add_to_history_buffer(G_z, cond_text_embed)]):
+        G_z = tf.identity(new_fake_batch)
+    G_z.set_shape([args.train_batch_size, _WINDOW_LEN, 1])
+
+  G_vars = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, scope='G')
 
   # Print G summary
   print('-' * 80)
@@ -80,6 +84,9 @@ def train(fps, args):
   x_rms = tf.sqrt(tf.reduce_mean(tf.square(x[:, :, 0]), axis=1))
   tf.summary.histogram('x_rms_batch', x_rms)
   tf.summary.histogram('G_z_rms_batch', G_z_rms)
+  tf.summary.scalar('history_buffer_size', history_buffer.current_size)
+  tf.summary.scalar('from_history_size', tf.shape(clips_from_history)[0])
+  tf.summary.audio('G_z_history', clips_from_history, _FS, max_outputs=10)
   tf.summary.scalar('x_rms', tf.reduce_mean(x_rms))
   tf.summary.scalar('G_z_rms', tf.reduce_mean(G_z_rms))
   tf.summary.scalar('Conditional Resample - KL-Loss', c_kl_loss)
@@ -105,7 +112,10 @@ def train(fps, args):
 
   # Make fake / wrong discriminator
   with tf.name_scope('D_G_z'), tf.variable_scope('D', reuse=True):
-    D_G_z = WaveGANDiscriminator(G_z, **args.wavegan_d_kwargs)
+    # Need to make sure we use matching conditions from history for the discriminator
+    D_G_z_args = args.wavegan_d_kwargs.copy()
+    D_G_z_args['context_embedding'] = new_fake_embeds
+    D_G_z = WaveGANDiscriminator(G_z, **D_G_z_args)
   with tf.name_scope('D_w'), tf.variable_scope('D', reuse=True):
     D_w = WaveGANDiscriminator(wrong_audio, **args.wavegan_d_kwargs)
 
