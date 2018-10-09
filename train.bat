b1a10ab066ac036f7e7bacbae62f6745a9aace9b
FurryFur <lance.e.chaney@gmail.com>
2018-10-10 01:18:44 +1300

    batchnorm disabled = better performance. back to 128 embed as this doesn't affect performance.

--- train.bat
+++ train.bat
@@ -3,7 +3,6 @@ python train_wavegan.py train .\train ^
 --data_dir .\data\Final_Datasets\All ^
 --data_first_window ^
 --wavegan_genr_pp ^
---wavegan_batchnorm ^
 --wavegan_loss dcgan ^
 --wavegan_disc_phaseshuffle 0 ^
 --train_summary_secs 5
