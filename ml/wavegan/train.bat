b6680ff36eb8e0afc9c2f3b296df8cf1a82ce6f7
FurryFur <lance.e.chaney@gmail.com>
2018-10-10 04:55:55 +1300

    Switched to wgan-gp training.

--- ml/wavegan/train.bat
+++ ml/wavegan/train.bat
@@ -3,9 +3,7 @@ python train_wavegan.py train .\train ^
 --data_dir .\data\Final_Datasets\All ^
 --data_first_window ^
 --wavegan_genr_pp ^
---wavegan_loss dcgan ^
 --use_extra_uncond_loss ^
---wavegan_disc_nupdates 1 ^
 --train_summary_secs 5
 
 REM --use_extra_uncond_loss ^
