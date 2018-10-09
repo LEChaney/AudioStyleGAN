2363003e3b429a4d3128f576e74afd039ab3aaac
FurryFur <lance.e.chaney@gmail.com>
2018-10-10 07:58:07 +1300

    Revert "Disable history buffer for testing. Added extra small RMS statistic to discriminator."
    This reverts commit f872e30546c3ed928886abebc21d9575f40bc9cc.

--- train_wavegan.py
+++ train_wavegan.py
@@ -61,23 +61,23 @@ def train(fps, args):
   tf.summary.audio('G_z', G_z, _FS, max_outputs=10)
   tf.summary.text('Conditioning Text', cond_text[:10])
 
-  # with tf.variable_scope('G'):
-  #   # Make history buffer
-  #   history_buffer = HistoryBuffer(_WINDOW_LEN, args.train_batch_size * 100, args.train_batch_size)
+  with tf.variable_scope('G'):
+    # Make history buffer
+    history_buffer = HistoryBuffer(_WINDOW_LEN, args.train_batch_size * 100, args.train_batch_size)
 
-  #   # Select half of batch from history buffer
-  #   g_from_history, r_from_history, embeds_from_history = history_buffer.get_from_history_buffer()
-  #   new_fake_batch = tf.concat([G_z[:tf.shape(G_z)[0] - tf.shape(g_from_history)[0]], g_from_history], 0) # Use tf.shape to handle case when g_from_history is empty
-  #   new_cond_embeds = tf.concat([cond_text_embed[:tf.shape(cond_text_embed)[0] - tf.shape(embeds_from_history)[0]], embeds_from_history], 0)
-  #   new_real_batch = tf.concat([x[:tf.shape(x)[0] - tf.shape(r_from_history)[0]], r_from_history], 0)
-  #   with tf.control_dependencies([new_fake_batch, new_real_batch, new_cond_embeds]):
-  #     with tf.control_dependencies([history_buffer.add_to_history_buffer(G_z, x, cond_text_embed)]):
-  #       G_z = tf.identity(new_fake_batch)
-  #       x = tf.identity(new_real_batch)
-  #       args.wavegan_g_kwargs['context_embedding'] = tf.identity(new_cond_embeds)
-  #       args.wavegan_d_kwargs['context_embedding'] = args.wavegan_g_kwargs['context_embedding']
-  #   G_z.set_shape([args.train_batch_size, _WINDOW_LEN, 1])
-  #   x.set_shape([args.train_batch_size, _WINDOW_LEN, 1])
+    # Select half of batch from history buffer
+    g_from_history, r_from_history, embeds_from_history = history_buffer.get_from_history_buffer()
+    new_fake_batch = tf.concat([G_z[:tf.shape(G_z)[0] - tf.shape(g_from_history)[0]], g_from_history], 0) # Use tf.shape to handle case when g_from_history is empty
+    new_cond_embeds = tf.concat([cond_text_embed[:tf.shape(cond_text_embed)[0] - tf.shape(embeds_from_history)[0]], embeds_from_history], 0)
+    new_real_batch = tf.concat([x[:tf.shape(x)[0] - tf.shape(r_from_history)[0]], r_from_history], 0)
+    with tf.control_dependencies([new_fake_batch, new_real_batch, new_cond_embeds]):
+      with tf.control_dependencies([history_buffer.add_to_history_buffer(G_z, x, cond_text_embed)]):
+        G_z = tf.identity(new_fake_batch)
+        x = tf.identity(new_real_batch)
+        args.wavegan_g_kwargs['context_embedding'] = tf.identity(new_cond_embeds)
+        args.wavegan_d_kwargs['context_embedding'] = args.wavegan_g_kwargs['context_embedding']
+    G_z.set_shape([args.train_batch_size, _WINDOW_LEN, 1])
+    x.set_shape([args.train_batch_size, _WINDOW_LEN, 1])
 
   G_vars = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, scope='G')
 
