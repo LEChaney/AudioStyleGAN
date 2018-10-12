Testing with dcgan without weighting wrong loss.

loader.py
@@ -109,8 +109,8 @@ def get_batch(
 
   dataset = tf.data.TFRecordDataset(fps)
   dataset = dataset.map(_mapper, num_parallel_calls=cpu_count() // 2)
-  if repeat:
-    dataset = dataset.shuffle(buffer_size=buffer_size)
+  # if repeat:
+    # dataset = dataset.shuffle(buffer_size=buffer_size)
   dataset = dataset.batch(batch_size, drop_remainder=True)
   if repeat:
     dataset = dataset.repeat()

wavegan.py
@@ -218,7 +218,8 @@ def WaveGANGenerator(
 
   if (context_embedding is not None):
     # Reduce or expand context embedding to be size [embedding_dim]
-    c, kl_loss = sample_context_embeddings(context_embedding, embedding_dim, train)
+    c = compress_embedding(context_embedding, embedding_dim)
+    kl_loss = 0
     output = tf.concat([z, c], 1)
   else:
     output = z