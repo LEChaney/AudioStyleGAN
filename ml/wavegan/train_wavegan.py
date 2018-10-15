08f5060aaef293108f273b9940a7f037f7c9a144
FurryFur <lance.e.chaney@gmail.com>
2018-10-15 22:37:51 +1300

    getting global step from current session now uses previously calculated lod

--- ml/wavegan/train_wavegan.py
+++ ml/wavegan/train_wavegan.py
@@ -451,9 +451,10 @@ def train(fps, args):
     # Get the summary writer for writing extra summary statistics
     summary_writer = SummaryWriterCache.get(args.train_dir)
 
+    _lod = 0
     while True:
       # Calculate LOD
-      step = sess.run(tf.train.get_or_create_global_step(), feed_dict={lod: 0})
+      step = sess.run(tf.train.get_or_create_global_step(), feed_dict={lod: _lod})
       _lod = np.piecewise(step, [step < 2000, step >= 2000 and step < 3000,
                                  step >= 3000  and step < 5000,  step >= 5000  and step < 6000,
                                  step >= 6000  and step < 8000,  step >= 8000  and step < 9000,
