500c39705f6239875bb1e858bf7e3f31e6af1b5f
FurryFur <lance.e.chaney@gmail.com>
2018-10-19 11:53:58 +1300

    wgan-gp 0.0001 learning rate

--- train_wavegan.py
+++ train_wavegan.py
@@ -416,13 +416,13 @@ def train(fps, args):
         learning_rate=5e-5)
   elif args.wavegan_loss == 'wgan-gp':
     G_opt = tf.train.AdamOptimizer(
-        learning_rate=1e-4,
+        learning_rate=0.0001,
         beta1=0.0,
-        beta2=0.9)
+        beta2=0.99)
     D_opt = tf.train.AdamOptimizer(
-        learning_rate=1e-4,
+        learning_rate=0.0001,
         beta1=0.0,
-        beta2=0.9)
+        beta2=0.99)
   else:
     raise NotImplementedError()
 
