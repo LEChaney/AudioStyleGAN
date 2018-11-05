f35205a86949b8dfadb754be663ba580781e5e80
FurryFur <lance.e.chaney@gmail.com>
2018-11-06 10:22:24 +1300

    nn upsample.

--- train.bat
+++ train.bat
@@ -3,7 +3,7 @@ python train_wavegan.py train .\train ^
 --data_first_window ^
 --use_extra_uncond_loss ^
 --wavegan_kernel_len 8 ^
---wavegan_genr_upsample zeros ^
+--wavegan_genr_upsample nn ^
 --train_batch_size 50 ^
 --wavegan_disc_nupdates 1 ^
 --train_summary_secs 15
