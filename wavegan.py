Fixed incorrect usage of align corners with nearest neighbor upsample (not inversed by average downsample)

wavegan.py
@@ -57,7 +57,7 @@ def nn_upsample(inputs, stride=4):
   with tf.variable_scope('nn_upsample'):
     w = tf.shape(inputs)[1]
     output = tf.expand_dims(inputs, axis=1)
-    output = tf.image.resize_nearest_neighbor(output, [1, w * stride], align_corners=True)
+    output = tf.image.resize_nearest_neighbor(output, [1, w * stride])
     output = output[:, 0]
     return output
 