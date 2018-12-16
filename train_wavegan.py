925165e672329817a50c8ef14b91f65e6d1a56c7
FurryFur <lance.e.chaney@gmail.com>
2018-12-16 19:10:12 +1300

    Increased latent vector size and reduce learning rate.

--- train_wavegan.py
+++ train_wavegan.py
@@ -24,7 +24,7 @@ from functools import reduce
 """
 _FS = 16000
 _WINDOW_LEN = 16384
-_D_Z = 128
+_D_Z = 512
 
 
 """
@@ -417,11 +417,11 @@ def train(fps, args):
         learning_rate=5e-5)
   elif args.wavegan_loss == 'wgan-gp':
     G_opt = tf.train.AdamOptimizer(
-        learning_rate=4e-4,
+        learning_rate=1e-4,
         beta1=0.0,
         beta2=0.9)
     D_opt = tf.train.AdamOptimizer(
-        learning_rate=4e-4,
+        learning_rate=1e-4,
         beta1=0.0,
         beta2=0.9)
   else:
